d6346c20534adb7943fa2be457eb4b66b1ebae61
cskye97 <68037561+cskye97@users.noreply.github.com>
2020-08-31 12:12:27 -0500

    cleaned, commented

--- diceClasses.py
+++ diceClasses.py
@@ -19,35 +19,28 @@ from dicey_token import token
 Global variables
 """
 
-cRoll = "/croll"
-simpleRoll = "/roll"
-disconnect = "/disconnect"
-doc = "/help"
-simpleHelp = "/simplerollhelp"
-cRollHelp = "/cocrollhelp"
-
 fail = "Couldn't parse input. Use /help to get more information."
 
 simpleRollDoc = """
 ```
-/roll [[iterations]x][[number]d[die type]][+/-[bonus]][drop [number]][>[=]success boundary][![>[=] exploding dice][,[new roll]]
+/roll [[iterations]x][[number]d[die type]][+/-[bonus]][other options][,[new roll]]
 Defaults to rolling 1d20
-Spaces don't matter.
 
-In order (and order does matter; it's set by what order the operations make sense in):
 [[iterations]x] has global scope; it applies to everything after it and cannot be repeated. It causes the same input to be used again; /roll 2x performs /roll twice.
 [number] on its own is interpreted as a bonus to the default roll.
 [xdy] rolls a y-sided die x times and adds together the results. If x is ommitted, it defaults to 1.
 [+ or - bonus] adds or subtracts a fixed amount to the total.
-[drop] drops the lowest N rolls from the total, where N defaults to 1 or can be set by appending an integer. It resets to 0 unless a roll is being exactly repeated.
-[>[=]] sets a boundary above which rolls will be counted as a success, and reported as successes or failures instead of raw numbers. Use >None to reset it to nonexistence.
-[!] causes World of Darkness style exploding dice (rolls above a certain value trigger a new roll). It applies to the total xdy+z, not to each individual dy. It defaults to the maximum number; set a boundary with [>[=]]. Reset to nonexistence with !None.
-[,] allows you to enter a new roll. Instead of 1d20, defaults will be set by the previous roll.
+[drop] drops the lowest N rolls from the total, where N defaults to 1 or can be set by appending an integer. It resets to 0 unless a roll is being repeated.
+[keep] keeps the lowest N rolls, dropping the higher from the total, where N defaults to 1 or can be set by appending an integer. It resets to 0 unless a roll is being repeated.
+[>/<[=]] sets a boundary above or below which rolls will be counted as a success, and reported as successes or failures instead of raw numbers.
+[!] causes World of Darkness style exploding dice (rolls above a certain value trigger a new roll). It applies to the total xdy+z, not to each individual dy. It defaults to the maximum number; set a boundary with [>[=]] or [<[=]].
+[!!] causes Shadowrun or Riddle of Steel style exploding dice, in which a new roll is added to the previous value.
+[,] allows you to enter a new roll. Instead of 1d20, defaults will be set by the previous roll. If a new die type is entered, "fancy" attributes like drop go back to default.
 
 Examples:
 		/roll
 		2
-		1d20+0: 2
+		1d20: 2
 
 		/roll 2d10+2
 		13, 22
@@ -58,10 +51,10 @@ Examples:
 		32, 4, 4, 40, 14, 3
 		3d17+3:  (17 + 7 + 5) + 3 = 32
 		3d17+2:  (2) + 2 = 4 (dropped 1, 1)
-		1d4+0:  4
+		1d4:  4
 		3d17+3:  (15 + 16 + 6) + 3 = 40
 		3d17+2:  (12) + 2 = 14 (dropped 3, 8)
-		1d4+0:  3
+		1d4:  3
 ```
 """
 
@@ -128,7 +121,8 @@ class Roll:
 		self.rollsLimit = 50
 		self.digitLimit = 10000
 
-		self.__commands__ = r"(drop|keep|exp|explode|>=|=>|<=|=<|[\+!><=d ])"
+		self.__commands__ = r"(drop|keep|>=|=>|<=|=<|[\+!><=d ])"
+		self.__fail__ = "Couldn't parse input. Use /help to get more information."
 		self.__mult__ = "Command appears more than once."
 		self.__incompatible__ = "Incompatible commands used."
 		self.__overRolls__ = "Sorry... I'd rather not print that many rolls."
@@ -179,8 +173,8 @@ class Roll:
 
 	def resolve(self, depth=0):
 		"""
-		Creates a DiceResult object given an xdy+z - style roll.
-		That roll is contained in a Roll() class
+		Creates a DiceResult object given an xdy+z - style roll
+		contained in a Roll() class
 		"""
 
 		if depth > self.rollsLimit:
@@ -207,10 +201,12 @@ class Roll:
 				dropList.append(max(result))
 				result.remove(max(result))
 
+		# Add up
 		CombinedResult = sum(result) + self.bonus
 
 		# Explode
 		if self.explode is not None:
+			# Stacking explosions
 			if not self.__explodeFlag__:
 				if CombinedResult >= self.explode:
 					ret = self.resolve(depth + 1)
@@ -220,6 +216,7 @@ class Roll:
 					else:
 						retList.extend(ret)
 
+			# Adding explosions
 			else:
 				if CombinedResult <= self.explode:
 					ret = self.resolve(depth + 1)
@@ -229,6 +226,7 @@ class Roll:
 					else:
 						retList.extend(ret)
 
+		# Notate adding explosions correctly.
 		if depth == 0 and self.explodeType == "add":
 			for ret in retList[::-1]:
 				CombinedResult += ret.total
@@ -249,15 +247,17 @@ class Roll:
 				else:
 					success = "Failure"
 
-		# Construct description string
+		# Begin constructing description string
 		prelude = ""
 		if depth > 0:
 			prelude += "Explosion:  "
 
+		# Basic roll
 		prelude += str(self.dice) + "d" + str(self.type)
 		if self.bonus != 0:
 			prelude += " + " + str(self.bonus)
 
+		# Success indicator
 		if self.success is not None:
 			if not self.__lessThanFlag__:
 				prelude += ' \u2265 ' 
@@ -265,6 +265,7 @@ class Roll:
 				prelude += ' \u2264 '
 			prelude += str(self.success)
 
+		# Explosion indicator
 		if self.explode is not None:
 			if self.explodeType == "stack":
 				prelude += " ! "
@@ -277,23 +278,29 @@ class Roll:
 				prelude += "\u2264 "
 			prelude += str(self.explode)
 
+		# Show rolls made
 		prelude += ':  '
 		resultDesc = ' + '.join([str(i) for i in result])
 
+		# Show bonus dice
 		if self.bonus == 0:
 			bonusDesc = ''
 		else:
 			resultDesc = '(' + resultDesc
 			bonusDesc = ') + ' + str(self.bonus)
+
+			# Show added bonus dice properly
 			if self.explode is not None and len(retList) > 0:
 				bonusDesc += '*' + str(len(retList) + 1)
 		desc = resultDesc + bonusDesc
 
+		# Show total if applicable
 		if '+' in desc:
 			desc = prelude + desc + ' = ' + str(CombinedResult)
 		else:
 			desc = prelude + str(CombinedResult)
 
+		# Show dropped dice if applicable
 		if len(dropList) > 0:
 			desc += '  (dropped ' + ', '.join([str(i) for i in dropList]) + ')'
 
@@ -303,28 +310,33 @@ class Roll:
 		ret.rollList = result
 		ret.dropList = dropList
 
-		if success == 'Success':
-			if not self.__lessThanFlag__:
-				desc += '  \u2265 '
-			else:
-				desc += '  \u2264 '
-			desc += str(self.success
-				)
-			ret.title = success
-			ret.colour = ret.COL_HARD_SUCCESS
-		elif success == 'Failure':
-			desc += '  < ' + str(self.success)
-			ret.title = success
-			ret.colour = ret.COL_NORM_FAILURE
-		else:
-			ret.title = str(CombinedResult)
-		
+		# Set success indicators (if not an added explosion die)
+		ret.title = str(CombinedResult)
+		if not (depth > 0 and self.explodeType == "add"):
+			if success == 'Success':
+				if not self.__lessThanFlag__:
+					desc += '  \u2265 '
+				else:
+					desc += '  \u2264 '
+				desc += str(self.success
+					)
+				ret.title = success
+				ret.colour = ret.COL_HARD_SUCCESS
+
+			elif success == 'Failure':
+				desc += '  < ' + str(self.success)
+				ret.title = success
+				ret.colour = ret.COL_NORM_FAILURE
+				
+		# Clean up the description
 		ret.desc = desc.replace("+-", "-").replace("+ -", "- ")
 		
+		# Make sure exploded dice appear in the right order
 		retList.append(ret)
 		if depth == 0:
 			retList.reverse()
 
+			# Show added exploded dice properly
 			if self.explodeType == "add":
 				for ret in retList[1:]:
 					retList[0].desc += "\n" + ret.desc
@@ -342,7 +354,6 @@ class Roll:
 			message = self.message
 
 		# The whole thing is in a try and will return ValueError on failure
-		# if True:
 		try:
 
 			# Clean up input
@@ -368,147 +379,132 @@ class Roll:
 			# Loop through rolls
 			self.result = []
 			for n in range(iterations):
+
 				# Defaults are reset each iteration to ensure the same result
 				self.__reset__()
 
 				for roll in rolls:
+
 					# Regular expression matches all possible commands
 					# Note that order does matter to avoid collision
 					# between "drop" and "d"
 
 					commands = re.split(self.__commands__, roll)
+
+					# Clean up the result a bit.
 					commands = [item for item in commands if item != " "]
 					commands = [item for item in commands if item != ""]
 					commands = [item for item in commands if item != "+"]
 
-					temp = []
 					for n in range(len(commands)):
 						if commands[n] is "-":
 							commands[n] = commands[n] + commands[n+1]
-							temp.append(n+1)
-					commands = [commands[n] for n in range(len(commands)) if n not in temp]
 
+					commands = [item for item in commands if item != "-"]
 
-					print(commands)
 					# Now search through commands to apply each one.
+
 					# xdy+z syntax
 					if "d" in commands:
 						index = commands.index("d")
-						# Reset values to default - surveys indicate that 
-						# this is expected behaviour
+
+						# If a new roll type is set, reset values to
+						# default - surveys indicate that this is expected
+						# behaviour
 						self.__reset__()
 
 						if index != 0:
 							try:
 								self.dice = int(commands[index-1])
-							except:
+							except ValueError:
 								pass
 
 						self.type = int(commands[index+1])
 						try:
 							self.bonus = int(commands[index+2])
-						except:
+						except ValueError:
 							pass
 
-					# Or interpret a single number as the bonus
+					# If no d, interpret a single number as the bonus
 					else:
 						try:
 							self.bonus = int(commands[0])
-						except:
+						except ValueError:
 							pass
 
+					# On to more complicated problems - drop and keep
 					if "drop" in commands:
 						index = commands.index("drop")
 
+						# Check for duplicates
 						if index + 1 < len(commands):
 							if "drop" in commands[index+1:]:
 								self.result = self.__mult__
 								return self.result
 
+						# Check for non-default (1) drop value
 						try:
 							self.drop = int(commands[index+1])
 							if index + 2 < len(commands):
+								# Undo that if drop was in front
+								# of a "d" command
 								if commands[index+2] == "d":
 									self.drop = 1
-						except:
+						except ValueError:
 							self.drop = 1
 
+					# Redo for the "keep" command
 					if "keep" in commands:
 						index = commands.index("keep")
 
+						# Check for duplicates
 						if "drop" in commands:
 							self.result = self.__incompatible__
 							return self.result
 
+						# Check for duplicates
 						if index + 1 < len(commands):
 							if "keep" in commands[index+1:]:
 								self.result = self.__mult__
 								return self.result
 
+						# Set keep andd check for non-default
 						self.__keepFlag__ = True
 						try:
 							self.drop = int(commands[index+1])
 							if index + 2 < len(commands):
+								# Undo that if keep was in front
+								# of  "d" command
 								if commands[index+2] == "d":
 									self.drop = 1
-						except:
+						except ValueError:
 							self.drop = 1
 
-					# Three different explode commands
-					# Explosion 1
+					# Explosion
 					if "!" in commands:
 						index = commands.index("!")
 
 						# Check for compound exploding dice
-						if index+1 != len(commands):
-							if index + 1 < len(commands):				
-								if commands[index+1] == "!":
-									self.explodeType = "add"
-									index += 1
-								else:
-									self.explodeType = "stack"
+						if index + 1 < len(commands):				
+							if commands[index+1] == "!":
+								self.explodeType = "add"
+								index += 1
+							else:
+								self.explodeType = "stack"
 
-								if index + 1 < len(commands):
-									if "!" in commands[index+1:]:
-										self.result = self.__mult__
-										return self.result
+							# Check for duplicates
+							if index + 1 < len(commands):
+								if "!" in commands[index+1:]:
+									self.result = self.__mult__
+									return self.result
 
 						# Actually set explosion number
 						try:
 							self.explode = int(commands[index+1])
-						except:
+						except ValueError:
 							self.explode = self.dice * self.type + self.bonus
 
-							if index+1 != len(commands):
-								if ">" in commands[index+1]:
-									self.explode = int(commands[index+2])
-									if "=" not in commands[index+1]:
-										self.explode += 1
-
-								elif "<" in commands[index+1]:
-									self.explode = int(commands[index+2])
-									self.__explodeFlag__ = True
-									if "=" not in commands[index+1]:
-										self.explode -= 1
-					
-					# Explosion 2
-					if "exp" in commands or "explode" in commands:
-						try:
-							index = commands.index("exp")
-						except:
-							index = commands.index("explode")
-
-						if "!" in commands[index+1:] or "exp" in commands[index+1:] or "explode" in commands[index+1:]:
-							self.result = self.__mult__
-							return self.result
-
-						# Compound exploding dice not possible here
-						# Set explosion number
-						try:
-							self.explode = int(commands[index+1])
-						except:
-							self.explode = self.dice * self.type + self.bonus
-
+							# Look for comparison symbols
 							if index+1 != len(commands):
 								if ">" in commands[index+1]:
 									self.explode = int(commands[index+2])
@@ -526,15 +522,22 @@ class Roll:
 					# overlap with exploding dice
 					setSuccess = False
 					for i in range(len(commands)):
+						# Check for a comparison symbol
 						if ">" in commands[i] or "<" in commands[i]:
-							if i > 0 and commands[i-1] in ["drop", "keep", "!"]:
+							# Check that it's not found a !
+							if i > 0:
+								if commands[i-1] == "!":
 									continue
+
+							# Check for duplicates
 							elif setSuccess:
 								self.result = self.__mult__
 								return self.result
 
+							# Set success limit
 							setSuccess = True
 							self.success = int(commands[i+1])
+							# Check comparison symbol
 							if ">" in commands[i]:
 								if "=" not in commands[i]:
 									self.success += 1
@@ -544,12 +547,16 @@ class Roll:
 									self.success -= 1
 
 					# Check for bad numbers
+					# Might add more to these later.
+					# Too many digits
 					if any([abs(i[1]) > self.digitLimit for i in self.params().items() if type(i[1]) is int]):
 						self.result = self.__overDigits__
 						return self.result
+					# Too many rolls
 					if self.dice > self.rollsLimit:
 						self.result = __overRolls__
 						return self.result
+					# Infini-explode
 					if self.explode is not None:
 						if self.dice * self.type + self.bonus - self.explode == 1:
 							self.result = self.__badExplode__
@@ -560,9 +567,8 @@ class Roll:
 					if type(res) is str:
 						self.result = res
 						return self.result
-
-					self.result.extend(res)
-
+					else:
+						self.result.extend(res)
 
 			return self.result
 
